d7bc916e160a3b7a9fa0f7bb074b9fee7cfcb733
Richard Mahn <richard_mahn@wycliffeassociates.org>
2016-09-01 18:55:40 -0600

    Updated install-requirements.sh

--- install-requirements.sh
+++ install-requirements.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-functions=("request" "messageLog", "jobStarted", "jobCompleted", "jobFailed")
+#functions=("request" "messageLog", "jobStarted", "jobCompleted", "jobFailed")
+functions=("request")
 
 for function in "${functions[@]}"
 do
