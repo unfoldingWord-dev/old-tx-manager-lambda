50e60f7f1ca7c39129a1754b9c67662392eb35eb
Richard Mahn <richard_mahn@wycliffeassociates.org>
2016-09-23 23:21:21 -0600

    Update to test functions

--- functions/startJob/main.py
+++ functions/startJob/main.py
@@ -7,13 +7,10 @@ from tx_manager.tx_manager import TxManager
 def handle(event, context):
     print("------------PROCESSING DB STREAM---------------------")
     for record in event['Records']:
-        try:
-            if record['eventName'] == 'INSERT' and 'job_id' in record['dynamodb']['Keys']:
-                print(record['eventID'])
-                print(record['eventName'])
-                # print("DynamoDB Record: " + json.dumps(record['dynamodb'], indent=2))
-                job_id = record['dynamodb']['Keys']['job_id']['S']
-                TxManager().start_job(job_id)
-        except Exception:
-            pass
-        print("------------END PROCESSING DB STREAM---------------------")
+       if record['eventName'] == 'INSERT' and 'job_id' in record['dynamodb']['Keys']:
+            print(record['eventID'])
+            print(record['eventName'])
+            # print("DynamoDB Record: " + json.dumps(record['dynamodb'], indent=2))
+            job_id = record['dynamodb']['Keys']['job_id']['S']
+            TxManager().start_job(job_id)
+    print("------------END PROCESSING DB STREAM---------------------")
